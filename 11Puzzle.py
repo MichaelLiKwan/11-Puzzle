ce7a4335ad602c3283de8ab759103bb674c9abff
Michael Kwan <mk6147@nyu.edu>
2020-10-13 19:50:09 -0400

    Added input text file and brief description of project expectations

--- 11Puzzle.py
+++ 11Puzzle.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+# n n n n
+# n n n n
+# n n n n
+#
+# m m m m
+# m m m m
+# m m m m
+# Figure 1. Input file with 9 lines.
+# ***************************
+# n n n n
+# n n n n
+# n n n n
+#
+# m m m m
+# m m m m
+# m m m m
+#
+# d (shallowest goal node when root node has a depth of 0)
+# N (total number of nodes generated)
+# A A A A A A A ………… (List of actions to be completed where A is an action in the set of {L,R,U,D} - left, right, up, down)(d elements)
+# f f f f f f f f …………….. (fvalues of the nodes on the way to the solution from root to goal)(d+1 elements)
+# Figure 2. Output file with 12 lines.
