13528944848cc3f63c0d62408c01945918130469
Michael Kwan <mk6147@nyu.edu>
2020-10-13 20:30:04 -0400

    write to output file

--- 11Puzzle.py
+++ 11Puzzle.py
@@ -21,19 +21,57 @@
 # f f f f f f f f …………….. (fvalues of the nodes on the way to the solution from root to goal)(d+1 elements)
 # Figure 2. Output file with 12 lines.
 
-readFile = open("input.txt", "r")
+#READ INPUT FILE ===================================================
+inputFile = open("input.txt", "r")
+initial = []
 state = []
 goal = []
+d = None
+N = None
+actions = []
+fVals = []
 
 #read first three lines as initial state
 for i in range(3):
-    state.append(readFile.readline().strip().split(" "))
+    initial.append(inputFile.readline().strip().split(" "))
 
 #skip the fourth line, which is blank
-readFile.readline()
+inputFile.readline()
 
 #read last three lines as goal state
 for i in range(3):
-    goal.append(readFile.readline().strip().split(" "))
+    goal.append(inputFile.readline().strip().split(" "))
 
-readFile.close()
+inputFile.close()
+#====================================================================
+
+#WRITE OUTPUT FILE ===================================================
+outputFile = open("output.txt", "w")
+
+#write initial state
+for i in range(3):
+    outputFile.write(" ".join(initial[i]))
+    outputFile.write("\n")
+outputFile.write("\n")
+
+#write goal state
+for i in range(3):
+    outputFile.write(" ".join(goal[i]))
+    outputFile.write("\n")
+outputFile.write("\n")
+
+#write d and N
+outputFile.write(d)
+outputFile.write("\n")
+outputFile.write(N)
+outputFile.write("\n")
+
+#write actions
+outputFile.write(" ".join(actions))
+outputFile.write("\n")
+
+#write fVals
+outputFile.write(" ".join(fVals))
+
+outputFile.close()
+#====================================================================
